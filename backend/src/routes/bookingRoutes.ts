8d8a82ca75c66560789c1624511f90e964649987
jizuuu <giovisoria3@gmail.com>
2025-03-20 00:02:03 +0800

    Added bookingDTO and BookingRoutes

--- backend/src/routes/bookingRoutes.ts
+++ backend/src/routes/bookingRoutes.ts
@@ -1,7 +1,47 @@
-//     .get('/booking', getAllBooking)
-//     .get('/booking/online', getOnlineBooking)
-//     .get('/booking/walkin', getWalkInBooking)
-//     .get('/booking/:id', getBookingByID)
-//     .post('/booking', createBooking)
-//     .put('/booking/:id', updateBooking)
-//     .delete('/booking/:id', deleteBooking);
+import { createRoute, OpenAPIHono, z } from "@hono/zod-openapi";
+import { db } from "../config/database";
+import { BookingsTable } from "../schemas/Booking";
+import { BookingDTO, GetBookingDTO } from "../dto/bookingDTO";
+
+export default new OpenAPIHono().openapi(
+  createRoute({
+    tags: ["Bookings"],
+    summary: "Get all bookings",
+    method: "get",
+    path: "/",
+    request: {
+      query: z.object({
+        limit: z.coerce.number().nonnegative().optional().default(50),
+        page: z.coerce.number().nonnegative().optional().default(1),
+      }),
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: GetBookingDTO.array(),
+          },
+        },
+        description: "Retrieve all bookings",
+      },
+      400: {
+        description: "Bad request!",
+      },
+    },
+  }),
+  async (c) => {
+    const { limit, page } = c.req.valid("query");
+
+    const bookings = await db.query.BookingsTable.findMany({
+      limit,
+      offset: (page - 1) * limit,
+    });
+
+    const allBookings = bookings.map((booking) => GetBookingDTO.parse(booking));
+
+    return c.json({
+      total: bookings.length,
+      items: allBookings,
+    });
+  }
+);
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,6 +1,7 @@
 import authRoutes from "./authRoutes";
 import userRoutes from "./userRoutes";
 import packageRoutes from "./packageRoutes";
+import bookingRoutes from "./bookingRoutes"
 
-export const routes = [["/auth", authRoutes], ["/users", userRoutes], ["/packages", packageRoutes]] as const;
+export const routes = [["/auth", authRoutes], ["/users", userRoutes], ["/packages", packageRoutes], ["/bookings", bookingRoutes]] as const;
 export type AppRoutes = (typeof routes)[number];
