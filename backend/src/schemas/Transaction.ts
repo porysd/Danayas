4db344a14c1472ad3018fa0289f38328277c6d8c
Asigha <philipdextergarcia@yahoo.com>
2025-05-02 04:57:58 +0800

    feat(transaction): update TransactionsTable schema
    - Added `refundStatus` and `remainingBalance` fields
    - Added `transactionStatus` with enum ["partially-paid", "paid", "voided"]

--- backend/src/schemas/Transaction.ts
+++ backend/src/schemas/Transaction.ts
@@ -9,5 +9,8 @@ import { UsersTable } from "./User";
 export const TransactionsTable = sqliteTable("TRANSACTION", {
     transactionId: integer('transactionId').primaryKey({ autoIncrement: true}),
     bookingId: integer('bookingId').references(() => BookingsTable.bookingId).notNull(),
+    transactionStatus: text("transactionStatus", {enum: ["partially-paid", "paid", "voided"]}).notNull(),
+    refundStatus: text("refundStatus", {enum: ["none", "pending", "refunded"],}).notNull().default("none"),
+    remainingBalance: real("remainingBalance").notNull(),
     createdAt: text('createdAt').notNull().default(sql`(current_timestamp)`),
 });
